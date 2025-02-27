46f34ff332ffcab40c9f7fba739b54b7798134c1
Marina Shiyan <marin26ms@gmail.com>
2025-02-27 13:15:58 +0200

    update gallery code

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,6 +3,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+let lightbox = new SimpleLightbox('.gallery a');
+
 export const renderGallery = images => {
   const gallery = document.querySelector('.gallery');
   gallery.innerHTML = '';
@@ -17,23 +19,24 @@ export const renderGallery = images => {
     return;
   }
 
-  images.forEach(image => {
-    const galleryItem = document.createElement('div');
-    galleryItem.classList.add('gallery-item');
-    galleryItem.innerHTML = `
+  const markup = images
+    .map(
+      image => `
+    <div class="gallery-item">
       <a href="${image.largeImageURL}">
         <img src="${image.webformatURL}" alt="${image.tags}" />
       </a>
       <div class="info">
-        <p>Likes<span>${image.likes}</span> </p>
+        <p>Likes<span>${image.likes}</span></p>
         <p>Views <span>${image.views}</span></p>
-        <p>Comments<span>${image.comments}</span> </p>
-        <p>Downloads<span>${image.downloads}</span> </p>
+        <p>Comments<span>${image.comments}</span></p>
+        <p>Downloads<span>${image.downloads}</span></p>
       </div>
-    `;
-    gallery.appendChild(galleryItem);
-  });
+    </div>
+  `
+    )
+    .join('');
 
-  const lightbox = new SimpleLightbox('.gallery a');
+  gallery.innerHTML = markup;
   lightbox.refresh();
 };
